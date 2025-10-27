cc2dc1f70230442d0a0164a27cd62371acb25826
testuser <test@test.test>
2025-10-27 11:00:14 +0200

    for un while cikls

---
+++ iterations.js
@@ -0,0 +1,20 @@
+let str = "";
+
+for (let i = 21; i <= 30; i++) {
+  str += i;
+}
+
+console.log(str);
+
+console.log('')
+
+
+
+let n = 30;
+
+let str2 = "";
+while (n > 21) {
+  str2 -= n;
+}
+
+console.log(result);
