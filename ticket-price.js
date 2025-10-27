9bda2d507d5ea973d2fbbf1dc1a71c6d507f82f8
testuser <test@test.test>
2025-10-27 11:04:14 +0200

    if else conditionals

---
+++ ticket-price.js
@@ -0,0 +1,13 @@
+let visitorAge = 12;
+let isStudent = true;
+
+if ( visitorAge <= 6 ) {
+    console.log("Biļete bez maksas")
+}
+ if ( isStudent = true) {
+    console.log("Biļete makša 5 Euro")
+ }
+
+ else {
+    console.log ("Biļete maksā 10 euro")
+ }
