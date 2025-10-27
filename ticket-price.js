c23883f5dcc141239c197c99cf6a601a71596505
testuser <test@test.test>
2025-10-27 11:33:51 +0200

    If else cikls fixed atkal

--- ticket-price.js
+++ ticket-price.js
@@ -3,7 +3,7 @@ let isStudent = true;
 
 if (visitorAge <= 6)
   console.log("Biļete bez maksas")
-else if (isStudent = true)
+else if (isStudent == true)
     console.log("Biļete makša 5 Euro")
 else
   console.log ("Biļete maksā 10 euro")
