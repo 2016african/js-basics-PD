2e32e87c12ada708c8560539c8b449c024e1f66f
testuser <test@test.test>
2025-10-27 11:19:55 +0200

    māsiva tipi, konsolē tiek izvadīts vārds

---
+++ people-array.js
@@ -0,0 +1,8 @@
+let people = [
+  { name: "Anna", age: 20, isStudent: true },
+  { name: "Jānis", age: 35, isStudent: false }
+];
+
+for (let i = 0; i < people.length; i++) {
+  console.log("Vārds: " + people[i].name);
+}
